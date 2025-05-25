60f92bc82a9fa959b1d44f8f76faac48d45a1dd3
Adan Oviedo <adan1oviedo@gmail.com>
2025-05-25 20:12:27 +0000

    code up to chapter1 video 6

--- chapter1_5.sql
+++ chapter1_5.sql
@@ -1,4 +1,5 @@
-with purchase_dates as(
+with 
+purchase_dates as(
   select case
       when payment_method = 'cash' then payment_at
       else payment_at + interval '1 month'
@@ -15,6 +16,38 @@ purchase as(
     sum(total_amount) as total_amount
   from purchase_dates
   GROUP BY date_part('year', actual_payment_at)
+),
+revenue_dates as(
+  select case
+      when payment_method = 'cash' then payment_at
+      else payment_at + interval '1 month'
+    end as actual_payment_at,
+    sum(quantity * price) as total_amount
+  from sales
+  group by case
+      when payment_method = 'cash' then payment_at
+      else payment_at + interval '1 month'
+    end
+),
+revenue as(
+  select date_part('year', actual_payment_at) as period_year,
+    sum(total_amount) as total_amount
+  from revenue_dates
+  GROUP BY date_part('year', actual_payment_at)
+),
+loan_in as(
+  select date_part('year', loan_at) as period_year,
+        sum(value) as total_amount
+  from loans
+  group by date_part('year', loan_at)
+),
+expenses as(
+select date_part('year', payment_date) as period_year,
+        - sum(amount) as total_amount
+  from payments
+  where payment_type in ('equipment', 'wage', 'rent', 'utility', 'tax', 'loan', 'interest')
+  group by date_part('year', payment_date)
 )
-select *
-from purchase
+
+
+select * from expenses
